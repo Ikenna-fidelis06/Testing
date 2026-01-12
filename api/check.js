840df27421e0f89a8d258fde115861f5d0fcfb61
Ikenna-fidelis06 <139623414+Ikenna-fidelis06@users.noreply.github.com>
2026-01-12 23:00:10 +0100

    check.js

--- api/check.js
+++ api/check.js
@@ -1,3 +1,5 @@
+import crypto from "crypto";
+
 export default function handler(req, res) {
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Only POST allowed" });
@@ -5,20 +7,23 @@ export default function handler(req, res) {
 
   const { regNumber } = req.body;
 
-  // SAMPLE DATABASE (SERVER-SIDE ONLY)
   const records = {
     "NAB12345": { name: "Ikenna", score: 78 },
     "NAB67890": { name: "Chinedu", score: 64 }
   };
 
-  if (records[regNumber]) {
-    return res.status(200).json({
-      success: true
-    });
-  } else {
+  if (!records[regNumber]) {
     return res.status(401).json({
       success: false,
       message: "Invalid registration number"
     });
   }
+
+  // create token
+  const token = crypto.randomBytes(16).toString("hex");
+
+  return res.status(200).json({
+    success: true,
+    token
+  });
 }
